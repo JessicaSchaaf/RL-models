b7d98a7301d7434069ece992a3978d3adb16c929
JessicaSchaaf <39050058+JessicaSchaaf@users.noreply.github.com>
2018-07-03 15:34:07 +0200

    Update fitRLStimNA.R

--- fitRLStimNA.R
+++ fitRLStimNA.R
@@ -154,21 +154,21 @@ nRep <- n  # Number of repetitions per association
 nPart <- N # Number of participants
 nRepNA <- nRepNA  # Person-specific number of repetitions per association (without missing responses)
 
-data <- list("nStim","nRep","nRepNA","nPart","C","R") # Data input for JAGS
+data2 <- list("nStim","nRep","nRepNA","nPart","C","R") # Data input for JAGS
 
 # List of sampling starting values to give to JAGS
-myinits <- list(
+myinits2 <- list(
   list(eta = rep(.1,N), betaAcc = rep(.1,N), pi = .1), # Starting values chain 1
   list(eta = rep(.5,N), betaAcc = rep(.5,N), pi = .5), # Starting values chain 2
   list(eta = rep(.9,N), betaAcc = rep(.9,N), pi = .9)   # Starting values chain 3
 )
 
-parameters <- c("strategy","eta","beta","pi","etaGroupMean","etaGroupPrecision","betaGroupMean","betaGroupPrecision")  # Parameters saved to check sampling results
+parameters2 <- c("strategy","eta","beta","pi","etaGroupMean","etaGroupPrecision","betaGroupMean","betaGroupPrecision")  # Parameters saved to check sampling results
 
 time <- proc.time()  # Keep track of the sampling time
 
 # Save all representative samples ((n.iter*n.chains - n.burnin)/n.thin) from the posterior distribution
-samples <- jags(data, inits=myinits, parameters,
+samples2 <- jags(data2, inits=myinits2, parameters2,
                 model.file ="modelHierarchicalPSRLStimNA.txt",
                 n.chains=3, n.iter=50000, n.burnin=50000/2, n.thin=50,
                 DIC=T, working.directory=bugsdir)
@@ -182,34 +182,34 @@ samples <- jags(data, inits=myinits, parameters,
 # --------------------------------------------------------- #
 
 # Save samples in separate objects
-etaEst <- samples$BUGSoutput$sims.list$eta
-betaEst <- samples$BUGSoutput$sims.list$beta
-piEst <- samples$BUGSoutput$sims.list$pi
-etaGroupMeanEst <- samples$BUGSoutput$sims.list$etaGroupMean
-etaGroupPrecisionEst <- samples$BUGSoutput$sims.list$etaGroupPrecision
-betaGroupMeanEst <- samples$BUGSoutput$sims.list$betaGroupMean
-betaGroupPrecisionEst <- samples$BUGSoutput$sims.list$betaGroupPrecision
-strategyEst <- samples$BUGSoutput$sims.list$strategy
+etaEst2 <- samples2$BUGSoutput$sims.list$eta
+betaEst2 <- samples2$BUGSoutput$sims.list$beta
+piEst2 <- samples2$BUGSoutput$sims.list$pi
+etaGroupMeanEst2 <- samples2$BUGSoutput$sims.list$etaGroupMean
+etaGroupPrecisionEst2 <- samples2$BUGSoutput$sims.list$etaGroupPrecision
+betaGroupMeanEst2 <- samples2$BUGSoutput$sims.list$betaGroupMean
+betaGroupPrecisionEst2 <- samples2$BUGSoutput$sims.list$betaGroupPrecision
+strategyEst2 <- samples2$BUGSoutput$sims.list$strategy
 
 # Check convergence of sampling chains
-#range(samples$BUGSoutput$summary[,'Rhat'])
-#length(which(samples$BUGSoutput$summary[,'Rhat'] >= 1.1)) # Check how many sampling chains did not converge
-#which(samples$BUGSoutput$summary[,'Rhat'] >= 1.1)  # Check which sampling chains did not converge
+#range(samples2$BUGSoutput$summary[,'Rhat'])
+#length(which(samples2$BUGSoutput$summary[,'Rhat'] >= 1.1)) # Check how many sampling chains did not converge
+#which(samples2$BUGSoutput$summary[,'Rhat'] >= 1.1)  # Check which sampling chains did not converge
 
 # Compute the estimated parameter values over all samples
-etaEstim <- apply(etaEst,2,mean)
-betaEstim <- apply(betaEst,2,mean)
-piEstim <- mean(piEst)
-etaGroupMeanEstim <- mean(etaGroupMeanEst)
-etaGroupPrecisionEstim <- mean(etaGroupPrecisionEst)
-betaGroupMeanEstim <- mean(betaGroupMeanEst)*10
-betaGroupPrecisionEstim <- mean(betaGroupPrecisionEst)
-strategyEstim <- sapply(list(strategyFirstStim=apply(strategyEst[,,1],2,mean),
-                             strategySecondStim=apply(strategyEst[,,2],2,mean),
-                             strategyThirdStim=apply(strategyEst[,,3],2,mean),
-                             strategyFourthStim=apply(strategyEst[,,4],2,mean)),round)
+etaEstim2 <- apply(etaEst2,2,mean)
+betaEstim2 <- apply(betaEst2,2,mean)
+piEstim2 <- mean(piEst2)
+etaGroupMeanEstim2 <- mean(etaGroupMeanEst2)
+etaGroupPrecisionEstim2 <- mean(etaGroupPrecisionEst2)
+betaGroupMeanEstim2 <- mean(betaGroupMeanEst2)*10
+betaGroupPrecisionEstim2 <- mean(betaGroupPrecisionEst2)
+strategyEstim2 <- sapply(list(strategyFirstStim=apply(strategyEst2[,,1],2,mean),
+                             strategySecondStim=apply(strategyEst2[,,2],2,mean),
+                             strategyThirdStim=apply(strategyEst2[,,3],2,mean),
+                             strategyFourthStim=apply(strategyEst2[,,4],2,mean)),round)
 
-DICHierPSRL <- samples$BUGSoutput$DIC
+DICHierPSRL <- samples2$BUGSoutput$DIC
 
 save.image(file='Insert name for results here.RData')
 
